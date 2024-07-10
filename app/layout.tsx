9b3e609f8e7246da1245709b1aa743d103c535ee
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-07-10 17:24:38 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { Nunito } from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "VacationHub",
+  description: "VacationHub",
+};
+
+const font = Nunito({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
