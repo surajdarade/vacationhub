b13e5526650fa5634a144e24b10d643f07c0920f
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-07-14 11:49:47 +0530

    react fragment for page/propertis

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -29,6 +29,7 @@ const PropertiesPage = async () => {
   }
 
   return (
+    <>
     <div className="main-container">
       <Heading title="Properties" subtitle="List of your properties" backBtn />
       <div className="mt-8 md:mt-10 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 md:gap-8 gap-4">
@@ -55,6 +56,7 @@ const PropertiesPage = async () => {
         ) : null}
       </div>
     </div>
+    </>
   );
 };
 
